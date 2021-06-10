e81e3b3c53857d533a9d235a75e8fa425351df40
C. Burton <cjamesburton@outlook.com>
2021-06-10 20:24:00 +0100

    added signals

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/.config/dwmblocks/scripts/cmus",		30, 0},
-	{"", "~/.config/dwmblocks/scripts/btc GBP",		30, 0},
-	{" ", "~/.config/dwmblocks/scripts/disk",	30, 0},
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{" ", "acpi | awk '{print $4}' <(acpi) | sed 's/,//'",	30,		0 },
-	{" ", "~/.config/dwmblocks/scripts/volume",	30,  0},
-	{" ", "date '+%A %d/%m/%y  %H:%M'",		5,		0},
+	/*Icon*/	/*Command*/													/*Update Interval*/	/*Update Signal*/
+	{"",		"~/.config/dwmblocks/scripts/cmus",							5,					1},
+	{"",		"~/.config/dwmblocks/scripts/btc GBP",						30,					0},
+	{" ",		"~/.config/dwmblocks/scripts/disk",							360,				0},
+	{" ",		"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,					0},
+	{" ",		"acpi | awk '{print $4}' <(acpi) | sed 's/,//'",			30,					0},
+	{" ",		"~/.config/dwmblocks/scripts/volume",						0,					2},
+	{" ",		"date '+%A %d/%m/%y  %H:%M'",								30,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
