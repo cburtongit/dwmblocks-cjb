538f883e2ec625d18e389568cf95bb1b4be53378
C. Burton <cjamesburton@outlook.com>
2021-11-15 12:35:17 +0000

    STOP FUCKING ASKING ME FOR COMMIT MESSAGES YOU CUNT

--- blocks.h
+++ blocks.h
@@ -1,9 +1,9 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/													/*Update Interval*/	/*Update Signal*/
-	{" 🔊 ",		"~/.config/dwmblocks/scripts/volume",						0,					2},
-	{" 🔋 ",		"acpi | awk '{print $4}' <(acpi) | sed 's/,//'",			30,					0},
-	{" 📅 ",		"date '+%A %d.%m.%y  🕒 %H:%M'",							30,					0},
+	{"🔉 ",		"~/.config/dwmblocks/scripts/volume",						0,					2},
+	{"🔋 ",		"acpi | awk '{print $4}' <(acpi) | sed 's/,//'",			30,					0},
+	{"📅 ",		"date '+%A %d.%m.%y 🕒 %H:%M'",							30,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
