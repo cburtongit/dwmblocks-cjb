7a103d9ec8f48d78c8aac4d093b06c4965f5fbc3
C. Burton <cjamesburton@outlook.com>
2021-12-15 00:40:41 +0000

    update

--- blocks.h
+++ blocks.h
@@ -3,10 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/													/*Update Interval*/	/*Update Signal*/
 	{"🔉 ",		"~/.config/dwmblocks/scripts/volume",						0,					2},
 	{"🔋 ",		"acpi | awk '{print $4}' <(acpi) | sed 's/,//'",			30,					0},
-	{"📅 ",		"date '+%d.%m.%y  🕒 %H:%M'",							30,					0},
+	{"📅 ",		"date '+%A %d.%m.%y'",										30,					0},
+	{"🕒 ",		"date '+%H:%M'",											30,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 //static char delim[] = " | ";
-static char delim[] = "  ";
-static unsigned int delimLen = 3;
+static char delim[] = "    ";
+static unsigned int delimLen = 4;
